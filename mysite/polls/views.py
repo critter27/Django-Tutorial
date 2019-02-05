3291d6dad6e0a6f704c9863fe7ebeec5b68c1973
Ritter <1522851503@mil>
2019-02-05 11:29:01 -0500

    made a template for part 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello World. You're at the polls index bih.")
-
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])    
+    return HttpResponse(output)
+    #return HttpResponse("Hello World. You're at the polls index bih.")
+    
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s: " % question_id)
+    
+def results(request, question_id):
+    return HttpResponse("You're looking at the results for question %s: " % question_id)
+    
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s: " % question_id)
 # Create your views here.
