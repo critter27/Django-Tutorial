5c3d9ecd3f996cd52c689fee19e910758a1d24ec
Ritter <1522851503@mil>
2019-02-07 11:34:59 -0500

    listing 24, django 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Question
+from django.template import loader
+from django.shortcuts import render
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])    
-    return HttpResponse(output)
+    #template = loader.get_template('polls/index.html')
+    #output = ', '.join([q.question_text for q in latest_question_list])   
+    context = {'latest_question_list': latest_question_list}
+    #return HttpResponse(template.render(context,'polls/index.html',request))
     #return HttpResponse("Hello World. You're at the polls index bih.")
+    return render(request,'polls/index.html',context)
     
 def detail(request, question_id):
+    try:
+        Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
     return HttpResponse("You're looking at question %s: " % question_id)
     
 def results(request, question_id):
